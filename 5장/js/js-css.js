b3cea1bdbeca25d4371c42b4740be22c7eeefff8
noah-wilson0 <127368908+noah-wilson0@users.noreply.github.com>
2023-10-04 15:21:56 +0900

    1004 JavaScript

---
+++ 5장/js/js-css.js
@@ -0,0 +1,16 @@
+//제목 가져오기
+const title=document.querySelector("#title") 
+
+
+// 아이디가 title인 요소를 클릭하면 배경색과 글자색 수정
+title.onclick=()=>{
+    title.style.backgroundColor="pink";
+    title.style.color="red";
+}
+
+
+
+
+
+
+
